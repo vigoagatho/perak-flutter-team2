d93e6233b3e2c1b374c5d3ad0c89705f959a9b9d
vigoagatho <h1101201045@student.untan.ac.id>
2023-11-27 13:10:30 +0700

    commenting nda penting

--- lib/data/anime-list.dart
+++ lib/data/anime-list.dart
@@ -1,42 +1,42 @@
-class AnimeList {
-  final List<String> gambartren = [
-    'https://asset-2.tstatic.net/kaltim/foto/bank/images/haikyuu-to-the-top.jpg',
-    'https://media.hitekno.com/thumbs/2022/07/29/19456-bleach/730x480-img-19456-bleach.jpg',
-    'https://media.sukabumiupdate.com/media/2023/04/06/1680772425_642e8d490becb_YeQQqt5wjGNDyJWKHiIU.webp',
-    'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg'
-  ];
-  final List<String> judultren = [
-    'Haikyuu',
-    'Bleach',
-    'Kimetsu No Yaiba',
-    'Blue Lock'
-  ];
-  final List<String> gambarrekomen = [
-    'https://www.animationxpress.com/wp-content/uploads/2022/08/one-punch-man.jpg',
-    'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg',
-    'https://www.thestreambible.com/wp-content/uploads/2022/09/dragon-ball-Z-768x395.jpg',
-    'https://animecollective.com/wp-content/uploads/2020/09/hunter-x-hunter-arcs-ranked-1536x1024.jpg.webp'
-  ];
-  final List<String> judulrekomen = [
-    'One Punch Man',
-    'Blue Lock',
-    'Dragon Ball Z',
-    'Hunter x Hunter'
-  ];
-  final List<String> gambarlanjut = [
-    'https://cdn.antaranews.com/cache/1200x800/2022/08/22/jjk.jpg',
-    'https://www.viu.com/ott/id/articles/wp-content/uploads/2021/12/anime-attack-on-titan-final-season-part-2-key-visual-viu.jpg',
-    'https://www.animationxpress.com/wp-content/uploads/2022/08/one-punch-man.jpg',
-    'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg',
-    'https://asset-2.tstatic.net/kaltim/foto/bank/images/haikyuu-to-the-top.jpg',
-  ];
-  final List<String> judullanjut = [
-    'Jujutsu Kaisen',
-    'Attack on Titan',
-    'One Punch Man',
-    'Blue Lock',
-    'Haikyuu'
-  ];
-  final List<String> episode = ['7', '23', '25', '14', '21'];
-  final List<String> persen = ['30%', '70%', '20%', '45%', '90%'];
-}
+// class AnimeList {
+//   final List<String> gambartren = [
+//     'https://asset-2.tstatic.net/kaltim/foto/bank/images/haikyuu-to-the-top.jpg',
+//     'https://media.hitekno.com/thumbs/2022/07/29/19456-bleach/730x480-img-19456-bleach.jpg',
+//     'https://media.sukabumiupdate.com/media/2023/04/06/1680772425_642e8d490becb_YeQQqt5wjGNDyJWKHiIU.webp',
+//     'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg'
+//   ];
+//   final List<String> judultren = [
+//     'Haikyuu',
+//     'Bleach',
+//     'Kimetsu No Yaiba',
+//     'Blue Lock'
+//   ];
+//   final List<String> gambarrekomen = [
+//     'https://www.animationxpress.com/wp-content/uploads/2022/08/one-punch-man.jpg',
+//     'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg',
+//     'https://www.thestreambible.com/wp-content/uploads/2022/09/dragon-ball-Z-768x395.jpg',
+//     'https://animecollective.com/wp-content/uploads/2020/09/hunter-x-hunter-arcs-ranked-1536x1024.jpg.webp'
+//   ];
+//   final List<String> judulrekomen = [
+//     'One Punch Man',
+//     'Blue Lock',
+//     'Dragon Ball Z',
+//     'Hunter x Hunter'
+//   ];
+//   final List<String> gambarlanjut = [
+//     'https://cdn.antaranews.com/cache/1200x800/2022/08/22/jjk.jpg',
+//     'https://www.viu.com/ott/id/articles/wp-content/uploads/2021/12/anime-attack-on-titan-final-season-part-2-key-visual-viu.jpg',
+//     'https://www.animationxpress.com/wp-content/uploads/2022/08/one-punch-man.jpg',
+//     'https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/08/Blue-Lock-TV-Anime.jpg',
+//     'https://asset-2.tstatic.net/kaltim/foto/bank/images/haikyuu-to-the-top.jpg',
+//   ];
+//   final List<String> judullanjut = [
+//     'Jujutsu Kaisen',
+//     'Attack on Titan',
+//     'One Punch Man',
+//     'Blue Lock',
+//     'Haikyuu'
+//   ];
+//   final List<String> episode = ['7', '23', '25', '14', '21'];
+//   final List<String> persen = ['30%', '70%', '20%', '45%', '90%'];
+// }
--- lib/data/data-image-slider.dart
+++ lib/data/data-image-slider.dart
@@ -1,12 +1,12 @@
-class DataImageSlider {
-  final List<String> imageList = [
-    'https://dorangadget.com/wp-content/uploads/2023/05/Karakter-One-Piece-63.jpg',
-    'https://oyster.ignimgs.com/wordpress/stg.ign.com/2021/02/jujutsu-kaisen.jpg',
-    'https://akcdn.detik.net.id/visual/2022/05/17/spy-x-family_169.png?w=650&q=90',
-  ];
-  final List<String> judul = [
-    'One Piece',
-    'Jujutsu Kaisen',
-    'Spy X Family',
-  ];
-}
+// class DataImageSlider {
+//   final List<String> imageList = [
+//     'https://dorangadget.com/wp-content/uploads/2023/05/Karakter-One-Piece-63.jpg',
+//     'https://oyster.ignimgs.com/wordpress/stg.ign.com/2021/02/jujutsu-kaisen.jpg',
+//     'https://akcdn.detik.net.id/visual/2022/05/17/spy-x-family_169.png?w=650&q=90',
+//   ];
+//   final List<String> judul = [
+//     'One Piece',
+//     'Jujutsu Kaisen',
+//     'Spy X Family',
+//   ];
+// }
